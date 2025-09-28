9a940167919e0f4a76967781e852bf8863b74a94
George Karountzos <george3-2001@hotmail.com>
2025-09-28 16:58:00 +0300

    warning fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Poppins } from "next/font/google";
 import { Outfit } from "next/font/google";
 import "./globals.css";
