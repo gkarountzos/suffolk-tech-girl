354448a756c9d3425af9ca2876df8c904cc2c578
George Karountzos <george3-2001@hotmail.com>
2025-09-28 17:08:26 +0300

    logo update

--- src/components/Navigation/Logo.tsx
+++ src/components/Navigation/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "../../../public/logo.png";
+import logo from "../../../public/home.png";
 
 export default function Logo() {
   return (
@@ -9,7 +9,7 @@ export default function Logo() {
         alt="Suffolk's Tech Girl Logo"
         width={300}
         height={207}
-        className="h-16 w-16 md:h-12 md:w-16 "
+        className="h-16 w-16 md:h-12 md:w-16 aspect-auto object-contain"
         priority
         quality={100}
       />
